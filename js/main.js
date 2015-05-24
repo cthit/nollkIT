77cc07a74b79720f54f6554fa317ba268e8d4e9c
Oscar Evertsson <oscar.evertsson@live.com>
2015-05-24 15:31:55 +0200

    Removed unnecessary code.

--- js/main.js
+++ js/main.js
@@ -2,27 +2,6 @@ $(document).ready(function(){
 
 	var screenWidth= $(window).width();
 
-	if(screenWidth <= 660){
-		$('#group-points').css({"display":"none"})
-	} else if (screenWidth <= 960){
-		$('#group-points').css({"display":"none"})
-	}
-
-	//FIX NAVIGATION DEPENDING ON SIZE
-	$(window).resize(function(){
-		
-		screenWidth = $(window).width();
-		
-		if(screenWidth <= 660){
-			$('#group-points').css({"display":"none"});
-		} else if(screenWidth <= 960){ 
-				$('#group-points').css({"display":"none"});
-		} else if(screenWidth > 960) {
-			$('#group-points').css({"display":"block"});
-		}
-		 
-	});
-
 	//MOBILE ON HAMBURGER CLICK START
 	$('#mobile-hamburger-container').on('click',function() {
 		if ($('#mobile-menu-container').hasClass('show')) {
@@ -35,7 +14,6 @@ $(document).ready(function(){
 			$('#hamburger').css({'display':'none'});
 			$('#mobile-menu-container').slideDown();
 			$('#mobile-menu-container').addClass('show');
-
 		}		
 	});
 	//MOBILE ON HAMBURGER CLICK END
