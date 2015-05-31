c822750a21c7b78f58def0627fbe9aaeb26900c7
Oscar Evertsson <oscar.evertsson@live.com>
2015-05-31 21:59:29 +0200

    The scheme doesn't cover the entire screen on smaller deviced when you enter the site...

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-
-	var screenWidth= $(window).width();
-
     // GLÖM INTE ATT LÄGGA TILL REFERENS PÅ NYA SIDAN SEDAN! GOOGLE DEV KOLLA
     // http://fullcalendar.io/docs/google_calendar/
     $('#calendar').fullCalendar({
@@ -23,6 +20,53 @@ $(document).ready(function(){
             googleCalendarId: 'vsslnmoph2ae1se32drhd92psc@group.calendar.google.com'
         }
     });
+    //Adds the arrows to the fullCalendar
+    $('.fc-toolbar').after('<div id="arrow-container">' +
+    '<i id="arrow-down" class="fa fa-chevron-down arrow"></i>' +
+    '<i id="arrow-up" class="fa fa-chevron-up arrow"></i></div>');
+
+    //Variable for the screen size
+    var screenWidth = $(window).width();
+    //Changes the view depending on what breakpoint
+    var changeAccordingToSize = function(){
+        if(screenWidth <= 960){
+            $('.fc-view-container').css({"display": "none"});
+        } else {
+            $('.fc-view-container').css({"display": "block"});
+        }
+    };
+    //Ran when script launched
+    changeAccordingToSize();
+
+    //If resize calls changeAccordingToSize with the new screenWidth
+    $(window).resize(function(){
+        screenWidth = $(window).width();
+        changeAccordingToSize();
+    });
+
+    //On click for arrow pointing down
+    $('#arrow-down').on("click",function(){
+        if ($('.fc-view-container').queue().length == 0){
+            $('.fc-view-container').slideDown()
+            $(this).css({'display':'none'});
+            $('#arrow-up').fadeIn(500);
+            //Necessary since fullCalendar bug. if you don't do this the calendar doesn't render properly.
+            $('#calendar').fullCalendar( 'changeView', 'month' );
+            $('#calendar').fullCalendar( 'changeView', 'agendaDay' );
+
+        }
+    });
+
+    //On click for arrow pointing up
+    $('#arrow-up').on("click",function(){
+        if ($('.fc-view-container').queue().length == 0) {
+            $('.fc-view-container').slideUp()
+            $(this).css({'display':'none'});
+            $('#arrow-down').fadeIn(500);
+        }
+    });
+
+
 
 	//MOBILE ON HAMBURGER CLICK START
 	$('#mobile-hamburger-container').on('click',function() {
