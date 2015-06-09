33d035006aef8fae8cce58dcf8e8afe58cfd9935
Oscar Evertsson <oscar.evertsson@live.com>
2015-06-09 16:36:14 +0200

    Added the calendar-modal-panel

--- front-page.php
+++ front-page.php
@@ -13,6 +13,20 @@
 <?php
 get_header();
 ?>
+
+<div class="calendar-modal-panel modal-panel">
+    <button id="calendar-close-modal-panel" class="close-modal-panel"><i class="fa fa-times"></i></button>
+    <h3 id="calendar-modal-panel-title">Title</h3>
+    <div id="calendar-modal-panel-content-container">
+        <ul class="list" id="where-when-how-list">
+            <li id="where">Var? Hubben 2.1</li>
+            <li id="when">När? xxx</li>
+            <li id="what">Vad? xxx</li>
+            <li ><a id="event-url" target="_blank" >Öppna detta event i google calendar <i class="fa fa-calendar"></i></a></li>
+        </ul>
+    </div>
+</div>
+
 <div class="content-container">
 
     <div id="group-points">
