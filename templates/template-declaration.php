0fb8b10907cc366a726a83e60a5a1f2bbe555fb0
Oscar Evertsson <oscar.evertsson@live.com>
2015-07-28 17:04:28 +0200

    Removed old comment.

--- templates/template-declaration.php
+++ templates/template-declaration.php
@@ -20,13 +20,6 @@
                 </article>
             </section>
         </div>
-        <!--
-        <div class="declaration-container">
-            <iframe src="https://docs.google.com/forms/d/1aNh7Wr43RbTM86ff2C89ogNBuB3M5RUHUO9-JWU1Sqc/viewform?embedded=true"
-                    width="100%" height="1450" frameborder="0" marginheight="0" marginwidth="0">Loading...
-            </iframe>
-        </div>
-        -->
     </div>
 
 
